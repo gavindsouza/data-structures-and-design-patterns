89c4ef07fb959001b8a2b80af2c36cf91e0c4656
Gavin D'souza <gavin18d@gmail.com>
2022-04-30 15:53:01 +0530

    feat(trie): Search words prefixed with X
    APIs in Trie class:
    * add - add word (str) to Trie
    * search - search for word (str) in Trie
    * like - search for words that start with word (str)
    * generate_words - generate all words with particular prefix or node

--- trie.py
+++ trie.py
@@ -2,11 +2,14 @@
 # simple trie implementation using dict
 
 
+from typing import Tuple, Union, List
+
+
 NULL = object()
 
 
 class Trie(dict):
-    def add(self, word):
+    def add(self, word: str) -> None:
         node = self
         _len = len(word)
 
@@ -20,14 +23,79 @@ class Trie(dict):
             node[letter] = next_node
             node = next_node
 
-    def search(self, word):
+    def _search(self, word: str) -> Tuple[str, Union["Trie", "None"]]:
         node = self
-        _len = len(word)
 
-        for i, letter in enumerate(word, 1):
-            if letter in node:
-                node = node[letter]
+        for letter in word:
+            if letter not in node:
+                return "", None
+            node = node[letter]
+        # if you've reached the end of the word, check
+        # if None exists to mark the end of the word
+        if None in node:
+            return word, node
+        return "", None
+
+    def _like(self, word: str) -> Tuple[List[str], Union["Trie", "None"]]:
+        node = self
+        first = True
+        for letter in word:
+            if letter not in node and not first:
+                return "", None
+            first = False
+            node = node[letter]
+        return self.generate_words(node=node), node
+
+    def search(self, word: str) -> bool:
+        """Search for a word in the trie.
+
+        Usage:
+            >>> from trie import Trie
+            >>> t = Trie()
+            >>> for name in ["gavin", "gavino", "stale", "stella", "stellar"]:
+            ...     t.add(name)
+            ...
+            >>> t.search("gavin")
+            True
+            >>> t.search("stale")
+            False
+        """
+        word, node = self._search(word=word)
+        return node is not None and node[None] is NULL
+
+    def like(self, word: str) -> List[str]:
+        """Generate all words in the trie that start with the given prefix.
+
+        Usage:
+            >>> from trie import Trie
+            >>> t = Trie()
+            >>> for name in ["gavin", "gavino", "stale", "stella", "stellar"]:
+            ...     t.add(name)
+            ...
+            >>> t.like("gav")
+            ['gavin', 'gavino']
+        """
+        endings, nodes = self._like(word=word)
+        return [f"{word}{ending}" for ending in endings]
+
+    def generate_words(self, prefix="", node=None) -> List[str]:
+        """Generate all words in the trie that start with the given prefix.
+
+        Usage:
+            >>> from trie import Trie
+            >>> t = Trie()
+            >>> for name in ["gavin", "gavino", "stale", "stella", "stellar"]:
+            ...     t.add(name)
+            ...
+            >>> t.generate_words(node=t['g'])
+            ['avin', 'avino']
+        """
+        node = node or self
+        words = []
+
+        for key in node:
+            if key is None:
+                words.append(prefix)
             else:
-                return False
-            if i == _len and None in node:
-                return node
+                words.extend(self.generate_words(prefix + key, node[key]))
+        return words
