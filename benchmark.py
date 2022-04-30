fb85cd8ff314d29a89229340ccdbc7804fe8e5c6
Gavin D'souza <gavin18d@gmail.com>
2022-04-30 18:33:52 +0530

    test: Benchmarking for trie with set & list
    Size of input dictionary: 966,550 words
    Size of test dictionary: 10,000 words
    
    Adding words to trie took 2.18e+00 seconds
    Searching in trie took 1.19e-02 seconds
    Prefixed search in trie took 9.88e+00 seconds
    Size of trie: 763 MB
    
    Adding words to set took 1.11e-01 seconds
    Searching in set took 8.28e-04 seconds
    Prefixed search in set took 1.26e-03 seconds
    Size of set: 80 MB
    
    Adding words to list took 2.34e-02 seconds
    Searching in list took 4.99e+01 seconds
    Prefixed search in list took 6.61e+02 seconds
    Size of list: 58 MB

---
+++ benchmark.py
@@ -0,0 +1,100 @@
+import sys
+import timeit
+from contextlib import contextmanager
+from random import choices, randint
+from string import ascii_letters
+from typing import List, Union
+
+from trie import Trie
+
+
+@contextmanager
+def time_this(var: str):
+    start_time = timeit.default_timer()
+    try:
+        yield
+    finally:
+        elapsed = timeit.default_timer() - start_time
+        print(f"{var} took {elapsed:.2e} seconds")
+
+
+def generate_random_word(length: int = None) -> str:
+    length = length or randint(1, 12)
+    return "".join(choices(ascii_letters, k=length))
+
+
+def get_size(obj, seen: Union[set, "None"] = None) -> int:
+    """Recursively finds size of objects"""
+    # from https://goshippo.com/blog/measure-real-size-any-python-object/
+
+    size = sys.getsizeof(obj)
+    if seen is None:
+        seen = set()
+    obj_id = id(obj)
+    if obj_id in seen:
+        return 0
+    # Important mark as seen *before* entering recursion to gracefully handle
+    # self-referential objects
+    seen.add(obj_id)
+    if isinstance(obj, dict):
+        size += sum([get_size(v, seen) for v in obj.values()])
+        size += sum([get_size(k, seen) for k in obj.keys()])
+    elif hasattr(obj, "__dict__"):
+        size += get_size(obj.__dict__, seen)
+    elif hasattr(obj, "__iter__") and not isinstance(obj, (str, bytes, bytearray)):
+        size += sum([get_size(i, seen) for i in obj])
+    return size
+
+
+def benchmark_trie(input_data: List[str], test_data: List[str]):
+    _trie = Trie()
+    _list = list()
+    _set = set()
+
+    with time_this("Adding words to trie"):
+        for word in input_data:
+            _trie.add(word)
+    with time_this("Searching in trie"):
+        for w in test_data:
+            _trie.search(w)
+    with time_this("Prefixed search in trie"):
+        for w in test_data:
+            _trie.like(w)
+    print(f"Size of trie: {get_size(_trie) // (1024 ** 2):,} MB\n")
+
+    with time_this("Adding words to set"):
+        for word in input_data:
+            _set.add(word)
+    with time_this("Searching in set"):
+        for w in test_data:
+            w in _set
+    with time_this("Prefixed search in set"):
+        for w in test_data:
+            {x for x in _list if x.startswith(w)}
+    print(f"Size of set: {get_size(_set) // (1024 ** 2):,} MB\n")
+
+    with time_this("Adding words to list"):
+        for word in input_data:
+            _list.append(word)
+    with time_this("Searching in list"):
+        for w in test_data:
+            w in _list
+    with time_this("Prefixed search in list"):
+        for w in test_data:
+            [x for x in _list if x.startswith(w)]
+    print(f"Size of list: {get_size(_list) // (1024 ** 2):,} MB\n")
+
+
+if __name__ == "__main__":
+    with open("../clojure-spellchecker/resources/words.txt") as f:
+        words = f.read().splitlines()
+    words.extend(generate_random_word() for _ in range(500_000))
+
+    non_existent_words = [generate_random_word() for _ in range(5_000)]
+    find_words = choices(words, k=5_000) + non_existent_words
+
+    print(f"Size of input dictionary: {len(words):,} words")
+    print(f"Size of test dictionary: {len(find_words):,} words")
+    print()
+
+    benchmark_trie(input_data=words, test_data=find_words)
